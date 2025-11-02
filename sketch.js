2416e8efa97d5c261054c35b1c0f5292181e588a
Hannes Altmann <hannes.altmann@uni-weimar.de>
2025-11-02 18:38:41 +0100

    Test nav scroll with javascript

--- sketch.js
+++ sketch.js
@@ -791,6 +791,12 @@
     }
   }
 
+function scrollToSection(id) {
+  const section = document.getElementById(id);
+  if (section) {
+    section.scrollIntoView({ behavior: 'smooth' });
+  }
+}
 
 
 
